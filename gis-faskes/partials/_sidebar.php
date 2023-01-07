9684b94e1e03b80e2ecd9ab7a616b2147f413ec4
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2023-01-07 23:08:35 +0700

    add fething api for get provinces

---
+++ gis-faskes/partials/_sidebar.php
@@ -0,0 +1,16 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="/gis/gis-faskes/index.php">
+                <span class="menu-title">Dashboard</span>
+                <i class="mdi mdi-home menu-icon"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/gis/gis-faskes/maps.php">
+                <span class="menu-title">Peta</span>
+                <i class="mdi mdi-book menu-icon"></i>
+            </a>
+        </li>
+    </ul>
+</nav>
