a0d59024d7b2f50acc34e41742eb4fc45034b96b
Danang TS <danangtskhair07islm@gmail.com>
2023-01-08 16:29:26 +0700

    add selected option province

--- gis-faskes/maps.php
+++ gis-faskes/maps.php
@@ -16,7 +16,7 @@
                           <span class="page-title-icon bg-gradient-primary text-white me-2">
                               <i class="mdi mdi-home"></i>
                           </span>
-                          Faskes Vaksinasi testing 123
+                          Faskes Vaksinasi Yang Tersedia
                       </h3>
                       <nav aria-label="breadcrumb">
                           <ul class="breadcrumb">
@@ -24,6 +24,30 @@
                                       class="mdi mdi-alert-circle-outline icon-sm text-primary align-middle"></i></li>
                           </ul>
                       </nav>
+                  </div>
+                  <div class="row">
+                      <div class="col-12 grid-margin stretch-card">
+                          <div class="card">
+                              <div class="card-body">
+                            <form >
+                                <div class="form-group">
+                                  <label for=""></label>
+                                  <select  id="province" class="form-select" aria-label="Default select example">
+                                <option selected>Pilih Provinsi</option>
+                              </select>
+                                </div>
+                              
+                                <div class="form-group">
+                                  <label for=""></label>
+                                  <select  id="city" class="form-select" aria-label="Default select example">
+                                <option selected>Pilih Kota</option>
+                              </select>
+                              </div>
+                              <button type="submit" class="btn btn-primary">Cari <i class="mdi mdi-map-marker-plus"></i></button>
+                              </form>
+                              </div>
+                          </div>
+                      </div>
                   </div>
                   <div class="row">
                       <div class="col-12 grid-margin stretch-card">
@@ -43,11 +67,12 @@
       include ('./partials/_footer.php');
       ?>
       <script>
-var map = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+        const batchTrack = document.getElementById("province");
+        var map = L.map('map').setView([51.505, -0.09], 13);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
 
 const getProvince = async () => {
     const url = 'https://kipi.covid19.go.id/api/get-province';
@@ -56,6 +81,19 @@ const getProvince = async () => {
     }
     const response = await fetch(url, config);
     const result = await response.json();
+    return result.results
+
 }
+const displayOption = async () => {
+  const options = await getProvince();
+  console.log(options);
+  for (option of options) {
+    const newOption = document.createElement("option");
+    newOption.value = option.key;
+    newOption.text = option.value;
+    batchTrack.appendChild(newOption);
+  }
+};
+displayOption();
       </script>
       <!-- partial -->
--- gis-faskes/partials/_sidebar.php
+++ gis-faskes/partials/_sidebar.php
@@ -1,13 +1,13 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <ul class="nav">
         <li class="nav-item">
-            <a class="nav-link" href="/gis/gis-faskes/index.php">
+            <a class="nav-link" href="./index.php">
                 <span class="menu-title">Dashboard</span>
                 <i class="mdi mdi-home menu-icon"></i>
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="/gis/gis-faskes/maps.php">
+            <a class="nav-link" href="./maps.php">
                 <span class="menu-title">Peta</span>
                 <i class="mdi mdi-book menu-icon"></i>
             </a>
