cec6b4960309ac16f4dae8fd30780a77f54459ec
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2023-01-10 10:56:02 +0700

    update logic select option for city

--- gis-faskes/maps.php
+++ gis-faskes/maps.php
@@ -29,22 +29,23 @@
                       <div class="col-12 grid-margin stretch-card">
                           <div class="card">
                               <div class="card-body">
-                            <form >
-                                <div class="form-group">
-                                  <label for=""></label>
-                                  <select  id="province" class="form-select" aria-label="Default select example">
-                                <option selected>Pilih Provinsi</option>
-                              </select>
-                                </div>
-                              
-                                <div class="form-group">
-                                  <label for=""></label>
-                                  <select  id="city" class="form-select" aria-label="Default select example">
-                                <option selected>Pilih Kota</option>
-                              </select>
-                              </div>
-                              <button type="submit" class="btn btn-primary">Cari <i class="mdi mdi-map-marker-plus"></i></button>
-                              </form>
+                                  <form>
+                                      <div class="form-group">
+                                          <label for=""></label>
+                                          <select id="province" class="form-select" aria-label="Default select example">
+                                              <option selected>Pilih Provinsi</option>
+                                          </select>
+                                      </div>
+
+                                      <div class="form-group">
+                                          <label for=""></label>
+                                          <select id="city" class="form-select" aria-label="Default select example">
+                                              <option selected>Pilih Kota</option>
+                                          </select>
+                                      </div>
+                                      <button type="submit" class="btn btn-primary">Cari <i
+                                              class="mdi mdi-map-marker-plus"></i></button>
+                                  </form>
                               </div>
                           </div>
                       </div>
@@ -67,13 +68,13 @@
       include ('./partials/_footer.php');
       ?>
       <script>
-        const id_province = document.getElementById("province");
-        const id_city = document.getElementById("city");
-        var map = L.map('map').setView([51.505, -0.09], 13);
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }).addTo(map);
+const id_province = document.getElementById("province");
+const id_city = document.getElementById("city");
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 const getProvince = async () => {
     const url = 'https://kipi.covid19.go.id/api/get-province';
@@ -86,13 +87,13 @@ const getProvince = async () => {
 
 }
 const provinceOption = async () => {
-  const options = await getProvince();
-  for (option of options) {
-    const newOption = document.createElement("option");
-    newOption.value = option.key;
-    newOption.text = option.value;
-    id_province.appendChild(newOption);
-  }
+    const options = await getProvince();
+    for (option of options) {
+        const newOption = document.createElement("option");
+        newOption.value = option.key;
+        newOption.text = option.value;
+        id_province.appendChild(newOption);
+    }
 };
 
 provinceOption();
@@ -109,13 +110,13 @@ const getCity = async () => {
     return result.results
 }
 const cityOption = async () => {
-  const options = await getCity();
-  for (option of options) {
-    const newOption = document.createElement("option");
-    newOption.value = option.key;
-    newOption.text = option.value;
-    id_city.appendChild(newOption);
-  }
+    const options = await getCity();
+    for (option of options) {
+        const newOption = document.createElement("option");
+        newOption.value = option.key;
+        newOption.text = option.value;
+        id_city.appendChild(newOption);
+    }
 };
 
 cityOption();
